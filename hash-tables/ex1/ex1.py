fba7afd71c9a695244014e380ee92ddf4fb9ceab
unknown <kyleteeter12@gmail.com>
2019-08-03 12:33:50 -0500

    Setting up basecase and looping.

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -12,6 +12,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # Set up base case
+    if length <= 1:
+        result = None
+
+    # Loop through weights and insert into hashtable
+    for i, weight in enumerate(weights):
+        hash_table_insert(ht, weight, i)
+        first_set = weight
+        second_set = limit - first_set
+        retrieve = hash_table_retrieve(ht, second_set)
+
 
     return None
 
